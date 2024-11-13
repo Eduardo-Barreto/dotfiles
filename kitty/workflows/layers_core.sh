5585eb5bb1624eb4a3b08dd1af966d967d844060
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2024-11-13 13:57:25 -0300

    :wrench: Update workflows

--- kitty/workflows/layers_core.sh
+++ kitty/workflows/layers_core.sh
@@ -9,9 +9,12 @@ kitty @ new-window --cwd ~/Layers/layers-core-id zsh -c "source ~/.zshrc && nvm 
 echo "Running core-app-web"
 kitty @ new-window --cwd ~/Layers/layers-core-app-web zsh -c "source ~/.zshrc && nvm use && yarn start; exec zsh"
 
+echo "Running erp-mock"
+kitty @ new-window --cwd ~/Layers/mock zsh -c "source ~/.zshrc && yarn start; exec zsh"
+
 echo "Running core-backend"
 cd ~/Layers/layers-core-backend
-sudo docker compose up -d
+docker compose up -d
 nvm use
 yarn start
 
---
+++ kitty/workflows/layers_visualizadores.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+source ~/.zshrc
+kitten @ set-tab-title "Run visualizadores"
+
+echo "Running visualizadores"
+cd ~/Layers/visualizadores-nuxt
+yarn dev 
+
