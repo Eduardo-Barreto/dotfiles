41f28367fd2ffc8e2db2b2d1cc4f6749c85e4dcb
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2025-01-25 12:29:53 -0300

    🎨 Refactor workflows

--- kitty/workflows/layers_core.sh
+++ kitty/workflows/layers_core.sh
@@ -4,13 +4,13 @@ source ~/.zshrc
 kitten @ set-tab-title "Run Core"
 
 echo "Running core-id"
-kitty @ new-window --cwd ~/Layers/layers-core-id zsh -c "source ~/.zshrc && nvm use && yarn start; exec zsh"
+kitty @ new-window --cwd ~/Layers/layers-core-id zsh -c "source ~/.zshrc && nvm use && yarn start; zsh"
 
 echo "Running core-app-web"
-kitty @ new-window --cwd ~/Layers/layers-core-app-web zsh -c "source ~/.zshrc && nvm use && yarn start; exec zsh"
+kitty @ new-window --cwd ~/Layers/layers-core-app-web zsh -c "source ~/.zshrc && nvm use && yarn start; zsh"
 
 echo "Running erp-mock"
-kitty @ new-window --cwd ~/Layers/mock zsh -c "source ~/.zshrc && yarn start; exec zsh"
+kitty @ new-window --cwd ~/Layers/mock zsh -c "source ~/.zshrc && yarn start; zsh"
 
 echo "Running core-backend"
 cd ~/Layers/layers-core-backend
---
+++ kitty/workflows/run_visualizador.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+APP_NAME=$1
+z $APP_NAME
+
+PROJECT_PATH=$(pwd)
+PROJECT_NAME=$(basename $PROJECT_PATH)
+
+kitten @ set-tab-title "Run $PROJECT_NAME"
+
+echo "Running web"
+kitty @ new-window --cwd "$PROJECT_PATH/web" zsh -c "source ~/.zshrc && nvm use && yarn start; zsh"
+
+echo "Running server"
+cd ./server/functions
+nvm use
+yarn serve
