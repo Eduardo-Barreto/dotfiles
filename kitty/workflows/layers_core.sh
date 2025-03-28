5f9e71ead73050b1b5c9d84313db56dbf237f1e9
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2025-03-28 09:21:29 -0300

    🔧 Remove nvm from workflows

--- kitty/workflows/layers_core.sh
+++ kitty/workflows/layers_core.sh
@@ -4,17 +4,16 @@ source ~/.zshrc
 kitten @ set-tab-title "Run Core"
 
 echo "Running core-id"
-kitty @ new-window --cwd ~/Layers/layers-core-id zsh -c "source ~/.zshrc && nvm use && yarn start; zsh"
+kitty @ new-window --cwd ~/Layers/layers-core-id zsh -c "source ~/.zshrc && yarn start; zsh"
 
 echo "Running core-app-web"
-kitty @ new-window --cwd ~/Layers/layers-core-app-web zsh -c "source ~/.zshrc && nvm use && yarn start; zsh"
+kitty @ new-window --cwd ~/Layers/layers-core-app-web zsh -c "source ~/.zshrc && yarn start; zsh"
 
-echo "Running erp-mock"
-kitty @ new-window --cwd ~/Layers/mock zsh -c "source ~/.zshrc && yarn start; zsh"
+echo "Running mock erp-modelo"
+kitty @ new-window --cwd "~/Layers/mock" zsh -c "source ~/.zshrc && yarn start; zsh"
 
 echo "Running core-backend"
 cd ~/Layers/layers-core-backend
 docker compose up -d
-nvm use
 yarn start
 
--- kitty/workflows/run_visualizador.sh
+++ kitty/workflows/run_visualizador.sh
@@ -9,9 +9,8 @@ PROJECT_NAME=$(basename $PROJECT_PATH)
 kitten @ set-tab-title "Run $PROJECT_NAME"
 
 echo "Running web"
-kitty @ new-window --cwd "$PROJECT_PATH/web" zsh -c "source ~/.zshrc && nvm use && yarn start; zsh"
+kitty @ new-window --cwd "$PROJECT_PATH/web" zsh -c "source ~/.zshrc && yarn start; zsh"
 
 echo "Running server"
 cd ./server/functions
-nvm use
 yarn serve
