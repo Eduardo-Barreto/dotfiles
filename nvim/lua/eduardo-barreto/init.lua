b023410e70697647d2158e035d67241630c5fbd6
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2025-03-28 09:21:50 -0300

    ✨ Add todo list bindings

--- nvim/lua/eduardo-barreto/init.lua
+++ nvim/lua/eduardo-barreto/init.lua
@@ -15,6 +15,35 @@ require("eduardo-barreto.options")
 require("eduardo-barreto.remap")
 require("lazy").setup("plugins")
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function(args)
+    vim.wo.wrap = true
+
+    vim.keymap.set('n', '<leader>n', 'o-[ ]  <esc>i', { buffer = args.buf, noremap = true, desc = "Add new task" })
+
+    vim.keymap.set('n', '<leader><cr>', function()
+      local line = vim.api.nvim_get_current_line()
+      if line:match('%-%[ %]') then
+        local new_line = line:gsub('%-%[ %]', '-[x]')
+        vim.api.nvim_set_current_line(new_line)
+      elseif line:match('%-%[x%]') then
+        local new_line = line:gsub('%-%[x%]', '-[ ]')
+        vim.api.nvim_set_current_line(new_line)
+      end
+    end, { buffer = true, noremap = true, silent = true, desc = "Toggle task" })
+
+    vim.api.nvim_create_autocmd('BufWinLeave', {
+      buffer = args.buf,
+      callback = function()
+        if vim.bo.filetype == 'markdown' then
+          vim.wo.wrap = false
+        end
+      end
+    })
+  end
+})
+
 function Colorize(color)
 	color = color or "catppuccin"
 	vim.cmd.colorscheme(color)
