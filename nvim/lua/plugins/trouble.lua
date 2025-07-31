f152a91eab5b81517362f79a64407b54adea247b
Eduardo-Barreto <eduardopontobarreto@gmail.com>
2025-07-30 22:22:33 -0300

    ✨ Add trouble
    folkeeee

---
+++ nvim/lua/plugins/trouble.lua
@@ -0,0 +1,17 @@
+return {
+  "folke/trouble.nvim",
+  opts = {}, -- for default options, refer to the configuration section for custom setup.
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+  },
+}
